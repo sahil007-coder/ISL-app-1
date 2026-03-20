b0873ae592a4a9a1f788e079d897499ecbf11cc2
Sahil Jasuja <jasuja773@gmail.com>
2026-03-20 15:41:43 +0530

    Fix Python version for TensorFlow compatibility

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+git add .
+git commit -m "fix python version for tensorflow"
+git push
